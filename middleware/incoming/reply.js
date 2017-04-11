f384e45787b05c45835af35f1b449ca6383275dc
Edward Ciggaar <edward_ciggaar@nl.ibm.com>
2017-04-11 15:13:27 +0200

    Implemented NLU call

--- middleware/incoming/reply.js
+++ middleware/incoming/reply.js
@@ -2,6 +2,7 @@ const watson = require('watson-developer-cloud');
 const config = require('../../config');
 const watsonConversationStorageMiddleware = require('../watson_conversation_storage');
 const watsonConversation = watson.conversation(config.watsonConversationCredentials);
+const watsonNaturalLanguageUnderstanding = new watson.NaturalLanguageUnderstandingV1(config.watsonNLUCredentials);
 
 const replyToUser = {
   type: 'incoming',
@@ -11,18 +12,40 @@ const replyToUser = {
 
     console.log(JSON.stringify(update));
 
-    const messageForWatson = {
-        context: update.session.context,
-        workspace_id: process.env.WATSON_WORKSPACE_ID,
-        input: {
-            text: update.message.text,
+    // Set options object when calling Watson Natural Language Understanding
+    const watsonNLUOptions = {
+        text: update.message.text,
+        features: {
+          sentiment: {},
+          emotion: {}
         }
-    };
+      };
 
-    watsonConversation.message(messageForWatson, (err, watsonUpdate) => {
-        watsonConversationStorageMiddleware.updateSession(update.sender.id, watsonUpdate);
-        const watsontext = watsonUpdate.output.text;
-        bot.sendTextCascadeTo(watsontext, update.sender.id);
+
+    watsonNaturalLanguageUnderstanding.analyze(watsonNLUOptions, (err, watsonNLUResponse) => {
+      if (err) {
+        console.log('Error occurred: ' + err); // An error occurred when invoking natural language understanding
+      } else {
+        // Updating the context with sentiment and emotion
+        update.session.context.sentiment = watsonNLUResponse.sentiment;
+        update.session.context.emotion = watsonNLUResponse.emotion;
+
+        // Use NLU enriched context in call to Watson Conversation
+        const messageForWatsonConversation = {
+            context: update.session.context,
+            workspace_id: process.env.WATSON_WORKSPACE_ID,
+            input: {
+                text: update.message.text,
+            }
+        };
+
+        // Calling Watson Conversation with enriched message object
+        watsonConversation.message(messageForWatsonConversation, (err, watsonUpdate) => {
+            watsonConversationStorageMiddleware.updateSession(update.sender.id, watsonUpdate);
+            const watsontext = watsonUpdate.output.text;
+            bot.sendTextCascadeTo(watsontext, update.sender.id);
+        });
+      }
     });
 
     next();
